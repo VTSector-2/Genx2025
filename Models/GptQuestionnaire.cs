e90ac5a99ec2ca0badd8c38d7b509b275a0b0d0d
himanshu-cbre <153586259+himanshu-cbre@users.noreply.github.com>
2025-03-26 13:03:49 +0530

    integrated GptService for the risk data.

---
+++ Models/GptQuestionnaire.cs
@@ -0,0 +1,11 @@
+﻿namespace Hackathon.Models
+{
+    public class GptQuestionnaire
+    {
+        public Dictionary<string, string> RiskProbabilityPrediction { get; set; } = new Dictionary<string, string>();
+        public Dictionary<string, string> RiskAnalysis { get; set; } = new Dictionary<string, string>();
+        public Dictionary<string, string> SummaryofRisks { get; set; } = new Dictionary<string, string>();
+        public Dictionary<string, string> Recommendations { get; set; } = new Dictionary<string, string>();
+        public Dictionary<string, string> AdditionalQuestions { get; set; } = new Dictionary<string, string>();
+    }
+}
